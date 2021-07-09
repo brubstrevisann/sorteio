62f3b6d9183c702461b621b7afa3c6092fcf0235
Brubs <bruno.amatoz@hotmail.com>
2021-07-08 22:30:29 -0300

    ajustes

--- src/app/services/sorteio-sindicato.service.ts
+++ src/app/services/sorteio-sindicato.service.ts
@@ -10,20 +10,20 @@ export class SorteioSindicatoService {
     private firestore: AngularFirestore
   ) { }
 
-/* create_NewIcecream : Cria um novo registro na coleção especificada usando o método add */
-create_newNumeroSorteado(record){
-  return this.firestore.collection('numeros_sorteados').add(record);
-}
-/*read_Icecream: Chama o método snapshotChanges , que obterá registros e também será registrado para receber atualizações */
-read_numerosSorteados(){
-  return this.firestore.collection('numeros_sorteados').snapshotChanges();
-}
-/*update_Icecream : atualiza o registro pegando o ID e chamando o método de atualização */
-update_Icecream(recordID,record) {
-  this.firestore.doc('Icecreams/' + recordID).update(record);
-}
-/*delete_Icecream : chama o método de exclusão  ao registrar o ID*/
-delete_Icecream(record_id) {
-  this.firestore.doc('Icecreams/' + record_id).delete();
+  /* create_NewIcecream : Cria um novo registro na coleção especificada usando o método add */
+  create_newNumeroSorteado(record) {
+    return this.firestore.collection('numeros_sorteados').add(record);
   }
+  /*read_Icecream: Chama o método snapshotChanges , que obterá registros e também será registrado para receber atualizações */
+  read_numerosSorteados() {
+    return this.firestore.collection('numeros_sorteados').snapshotChanges();
+  }
+  // /*update_Icecream : atualiza o registro pegando o ID e chamando o método de atualização */
+  // update_numerosSorteados(recordID,record) {
+  //   this.firestore.doc('numeros_sorteados/' + recordID).update(record);
+  // }
+  // /*delete_Icecream : chama o método de exclusão  ao registrar o ID*/
+  // delete_numerosSorteados(record_id) {
+  //   this.firestore.doc('numeros_sorteados/' + record_id).delete();
+  //   }
 }
